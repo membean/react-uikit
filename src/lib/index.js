60edb9b91964823df334d27b2825c825ddf55ba6
Brant Stuns <brantstuns@gmail.com>
2020-01-05 21:06:06 -0700

    refactored so the components can be published as a library

---
+++ src/lib/index.js
@@ -0,0 +1,29 @@
+import CheckBoxInput from "./components/CheckBoxInput/CheckBoxInput.js";
+import StandaloneCheckBoxInput from "./components/CheckBoxInput/StandaloneCheckBoxInput.js";
+import FileBrowserInput from "./components/FileBrowserInput/FileBrowserInput.js";
+import StandaloneFileBrowserInput from "./components/FileBrowserInput/StandaloneFileBrowserInput.js";
+import ProgressBar from "./components/ProgressBar/ProgressBar.js";
+import StandaloneProgressBar from "./components/ProgressBar/StandaloneProgressBar.js";
+import RadioGroupInput from "./components/RadioGroupInput/RadioGroupInput.js";
+import StandaloneRadioGroupInput from "./components/RadioGroupInput/StandaloneRadioGroupInput.js";
+import RadioButtonInput from "./components/RadioGroupInput/RadioButtonInput.js";
+import SelectInput from "./components/SelectInput/SelectInput.js";
+import StandaloneSelectInput from "./components/SelectInput/StandaloneSelectInput.js";
+import TextInput from "./components/TextInput/TextInput.js";
+import StandaloneTextInput from "./components/TextInput/StandaloneTextInput.js";
+
+export {
+  CheckBoxInput,
+  StandaloneCheckBoxInput,
+  FileBrowserInput,
+  StandaloneFileBrowserInput,
+  ProgressBar,
+  StandaloneProgressBar,
+  RadioGroupInput,
+  StandaloneRadioGroupInput,
+  RadioButtonInput,
+  SelectInput,
+  StandaloneSelectInput,
+  TextInput,
+  StandaloneTextInput
+};
